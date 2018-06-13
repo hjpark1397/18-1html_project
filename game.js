b0ed777ce77a15db5f574ab8b1734303a31f8081
Koobonik <kbi960130@naver.com>
2018-06-13 19:53:33 +0900

    오디오 재생 코드 추가 + 계란 클릭시 그림 바뀌는 기능 추가

--- game.js
+++ game.js
@@ -49,7 +49,7 @@ for (var i = 0; i < total; i++) {
 	q[i] = 0;
 	}
 	//정답 생성
-	while (tmp_a < a) {
+	while (tmp_a < a) { 
 		tmp_i = Math.floor(Math.random() * total);//랜덤함수로 난수 발생
 		if (q[tmp_i] == 0) {
 			q[tmp_i] = 1;  
@@ -65,21 +65,29 @@ for (var i = 0; i < total; i++) {
 document.getElementById("div_q").innerHTML = str;//HTML에 집어넣는다.
 console.log(q);
 
-function check(no) {//답을 골랐을 경우 메세지를 출력함
+function check(no) {//답을 골랐을 경우 메세지를 출력함 // q 는 정답을 담는 배열 변수임
     if (q[no] == 0) {
         alert("꽝");
+        var image = document.getElementById(no);
+        image.src = "boiled-egg.png"; // 계란후라이 보여줄 것
+        //sound(no); 오디오 넣고 나서 주석 풀어줘
     }
     else {
         alert("GOOD!");//이 자리에 이미지를 띄워주고 싶음...
+        var image = document.getElementById(no);
+        image.src = "chick.png"; // 살아있는 병아리 보여줄 것
+        //sound(no); 오디오 넣고 나서 주석 풀어줘
     }
 }
 
 function sound(no){//소리를 출력해줄 함수
     if(q[no]==0){
-        
+        var audio = new Audio('audio/clearsound.mp3'); //오디오 재생 코드 + 사용법 : '' 안에 경로 및 파일 이름 설정해주면 됨.
+        audio.play();
     }
     else{
-
+        var audio = new Audio('audio/clearsound.mp3'); //오디오 재생 코드
+        audio.play();
     }
 
 }
